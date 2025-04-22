2ecbc393f1d267e4cd09f005b02e2e71b631b880
ARBIND LOCHAN MISHRA <almbbsr456@gmail.com>
2025-04-22 18:22:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Home route to get client IP
+app.get('/', (req, res) => {
+  // This handles proxies like Heroku or AWS ELB
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  
+  res.send(`Your IP address is: ${ip}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
